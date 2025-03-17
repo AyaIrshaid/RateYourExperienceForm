eb73d72810ea6f4c5d237f4f3406e3d9bed1b344
AyaIrshaid <a.yirshaid@gmail.com>
2025-03-17 12:25:13 +0300

    Added Assets catalog

--- RateExperienceForm/RateExperienceForm/HomePage.swift
+++ RateExperienceForm/RateExperienceForm/HomePage.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct HomePage: View {
     var body: some View {
         VStack {
+            Image("star")
+                .frame(width: 50.0, height: 50.0)
             Text("Rate your experience!")
                 .foregroundColor(Color.white)
                 .font(.largeTitle)
